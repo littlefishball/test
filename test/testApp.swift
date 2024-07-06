7359e3156d67f2b58d6013ae70056296740f9f9d
phua hong yu <phua_hong_yu@students.edu.sg>
2024-07-06 11:40:38 +0800

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Tian peng Phua on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
